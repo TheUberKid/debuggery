96f6b6a4bc7be2d03fc4f3335f26b282ab509a9e
TheUberKid <zekixu@gmail.com>
2015-02-11 14:48:21 -0800

    Create registerform.php

---
+++ templates/registerform.php
@@ -0,0 +1,24 @@
+// NOTE: this html submits to register.php (debuggery/public) which has been omitted for security reasons.
+
+<div id="register">
+	<form action="register.php" method="post">
+	    <fieldset>
+	        <div class="form-group">
+	            <input autofocus class="form-control" name="username" placeholder="Username" type="text"/>
+	        </div>
+	        <div class="form-group">
+	            <input class="form-control" name="password" placeholder="Password" type="password"/>
+	        </div>
+	        <div class="form-group">
+	            <input class="form-control" name="confirmation" placeholder="Password (Confirm)" type="password"/>
+	        </div>
+	        <div class="submit-form">
+	            <button type="submit" class="btn btn-default">Register</button>
+	        </div>
+	    </fieldset>
+	</form>
+	<br>
+	<div>
+    or <a href="login.php">log in</a>
+	</div>
+</div>
