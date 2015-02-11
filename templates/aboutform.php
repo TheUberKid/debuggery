c77db9e8b063039c5f696aa234ab3c81445c6d8f
TheUberKid <zekixu@gmail.com>
2015-02-11 14:51:43 -0800

    Create aboutform.php

---
+++ templates/aboutform.php
@@ -0,0 +1,26 @@
+<div id = "aboutbody">
+	<div id = "aboutcaption">
+		About Debuggery
+	</div>
+	<div id= "aboutcontent">
+		<b>
+			Debuggery is an online free-to-use service that allows users from all over the world convene and work together to debug problems.
+		</b>
+		<br>
+		<p>
+			The goal of Debuggery is to create a community where programmers of any experience level can post bugs in their code for others to fix.
+			Registering for an account is free and helping sothers debug their programs is encouraged. All programming languages are accepted, and
+			points will be rewarded to those who post accepted fixes.
+		</p>
+		<p>
+			Criticism is frowned upon, but so are unclear or otherwise terrible questions. Be sure to include all relevent code and detail your problem clearly.
+			This site is <i>only for bugs that need fixing.</i> This is not <a href="http://stackoverflow.com/">StackOverflow</a>. We do not accept questions about
+			functionality or methods for certain actions. There are plenty of websites, code forums, etc. for you to use if you have a question of that nature.
+		</p>
+		<p>
+			We hope you will find use here. If you have any questions or concerns, either visit our <a href="/public/faq.php">FAQ</a> or <a href="/public/contact.php">Contact Us</a>.
+		</p>
+		<br>
+		<p> <a href="/public/register.php">Register</a> or <a href="/pubic/login.php">Login Here</a>.</p>
+	</div>
+</div>
