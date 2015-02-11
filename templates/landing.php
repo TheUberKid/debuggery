a7970ee6ef4f69f5df107f68a573e97579138eac
TheUberKid <zekixu@gmail.com>
2015-02-11 14:46:38 -0800

    Create landing.php

---
+++ templates/landing.php
@@ -0,0 +1,18 @@
+<div id="section">
+	<div id="innersection">
+		...Sometimes it just won't die.
+		<span class="quicklink">
+			<img src="/public/images/bug.png" height="35px" width="35px">
+		</span>
+		<span class="quicklink">
+			<a href="/public/about.php">
+		 		Learn More
+		 	</a>
+		</span>
+ 	</div>
+</div>
+
+<div id="video">
+	<iframe width="840" height="473" src="//www.youtube.com/embed/TESTING123" frameborder="0" allowfullscreen></iframe>
+	<div id="caption">Debuggery is an online free-to-use service that allows users from all over the world convene and work together to debug problems.</div>
+</div>
