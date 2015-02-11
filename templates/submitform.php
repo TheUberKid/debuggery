ebd1175e354d2f62d61ee458c1704f54fa46d6e3
TheUberKid <zekixu@gmail.com>
2015-02-11 14:51:13 -0800

    Create submitform.php

---
+++ templates/submitform.php
@@ -0,0 +1,24 @@
+<div id="aboutbody">
+	<div id="aboutcaption">
+		<center>Submit Code</center>
+	</div>
+	<center>Share your problems for the world to solve.</center>
+</div>
+
+<div id="submit">
+
+	<form action="submit.php" method="post">
+	    <fieldset>
+	        <div class="title-form">
+	            <input autofocus class="form-control-long" name="title" placeholder="Title" type="text"/>
+	        </div>
+	        <div class="title-form">
+	            <textarea class="form-textarea" name="desc" placeholder="Description - a short summary of your code"></textarea>
+	        </div>
+	        <div class="submit-form">
+	            <button type="submit" class="btn btn-default">Continue</button>
+	        </div>
+	    </fieldset>
+	</form>
+
+</div>
